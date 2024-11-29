de6d5ce8584f4a917dbab65aa7d01f83ab8d4389
boat718 <boat_loading@hotmail.com>
2024-11-29 22:18:46 +0700

    Initial project

---
+++ src/main/java/com/exercise/jobdataapi/JobDataApiApplication.java
@@ -0,0 +1,13 @@
+package com.exercise.jobdataapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobDataApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobDataApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exercise/jobdataapi/JobDataApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.jobdataapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobDataApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
