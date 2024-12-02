3b06c4c9d4487ee9cd3e5954bb8ebcff85b8c297
boat718 <boat_loading@hotmail.com>
2024-12-02 17:14:03 +0700

    delete unused method

--- src/main/java/com/exercise/jobdataapi/repository/JobDataRepository.java
+++ src/main/java/com/exercise/jobdataapi/repository/JobDataRepository.java
@@ -5,21 +5,7 @@ import com.exercise.jobdataapi.entity.JobData;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface JobDataRepository extends JpaRepository<JobData, Long>, JobDataRepositoryCustom {
-    List<JobData> findBySalaryGreaterThanEqualAndJobTitleContainingIgnoreCaseAndGender(Long salaryGreaterThanOrEqual, String jobTitle, String gender);
-
-    List<JobData> findBySalaryGreaterThanEqualAndJobTitleContainingIgnoreCase(Long salaryGreaterThanOrEqual, String jobTitle);
-
-    List<JobData> findBySalaryGreaterThanEqualAndGender(Long salaryGreaterThanOrEqual, String gender);
-
-    List<JobData> findByJobTitleContainingIgnoreCaseAndGender(String jobTitle, String gender);
-
-    List<JobData> findBySalaryGreaterThanEqual(Long salaryGreaterThanOrEqual);
-
-    List<JobData> findByGender(String gender);
-
-    List<JobData> findByJobTitleContainingIgnoreCase(String jobTitle);
 }
